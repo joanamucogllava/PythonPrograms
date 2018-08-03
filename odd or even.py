82345cee1b2abe211d3843298b9b37d55970b409
Joana Mucogllava <40967988+joanamucogllava@users.noreply.github.com>
2018-08-02 20:00:52 -0700

    New Project

---
+++ odd or even.py
@@ -0,0 +1,10 @@
+#This program will ask you to enter a number and
+#will state if the entered number is odd or even.
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("The number you entered is even.")
+else:
+    print("The number you entered is odd.")
+
