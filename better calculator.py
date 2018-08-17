01dbac8a5709151d6edd1d27b87a7ee5b5dc28bb
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-08-17 13:12:50 -0700

    Update better calculator.py

--- better calculator.py
+++ better calculator.py
@@ -1,3 +1,5 @@
+# This program will ask the user to enter 2 numberes and the operator that he wants to use.
+
 num1 = float(input("Enter the first number: "))
 op = input("Enter the operator: ")
 num2 = float(input("Enter the second number: "))
