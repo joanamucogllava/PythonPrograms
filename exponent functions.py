c4da39ed0d99e5c152ecb842b706c28f0875ede1
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-08-24 11:44:05 -0700

    Update exponent functions.py

--- exponent functions.py
+++ exponent functions.py
@@ -1,5 +1,5 @@
 # This program will raise the given base number to the given power number and print out the result.
-
+# Author: Joana Mucogllava
 
 def raise_to_power(base_num, pow_num):
     result = 1
