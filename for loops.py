e836da8c90a38378d4187830fe3501990d00d237
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-08-28 21:26:22 -0500

    New File

---
+++ for loops.py
@@ -0,0 +1,24 @@
+for letter in "Joana Mucogllava":
+    print(letter)
+
+
+friends = ["Joana", "Utku", "Nora"]
+
+for friend in friends:
+    print(friend)
+
+
+for index in range(10):
+    print(index)
+
+for index in range(6, 10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First Interation")
+    else:
+        print("Not first")
