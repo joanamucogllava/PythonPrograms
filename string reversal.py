5df87ed7c8aa7976a93c3298303cb504c9306569
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-09-05 21:57:16 -0700

    Update string reversal.py

--- string reversal.py
+++ string reversal.py
@@ -1,3 +1,6 @@
+# This program will ask the user to enter a word and after reversing it, it will determine if the word is palindrome or not.
+# Author: Joana Mucogllava
+
 wrd = input ("Please enter a word: ")
 word = str(wrd)
 rvs = wrd[::-1]
