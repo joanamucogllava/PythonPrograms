745dde4d20b81a63ebbcd8a0158f39aef2ca68a1
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-09-12 00:38:28 -0500

    New File

---
+++ Guessing_game2.py
@@ -0,0 +1,23 @@
+import random
+number = random.randint(0, 100)
+guesses = 0
+
+while 1:
+    guess = int(input("Enter an integer from 0 to 100: "))
+    guesses += 1
+    print ("This is your %d guess" %guesses)
+
+    if guess < number:
+        print("Your guess is low!")
+    elif guess > number:
+        print("Your guess is high!")
+    elif guess == number:
+        break
+    
+if guess == number:
+    guesses = str(guesses)
+    print("You guess it in :", guesses + " guesses")
+
+else:
+    number = str(number)
+    print("The secret number was ", number)
