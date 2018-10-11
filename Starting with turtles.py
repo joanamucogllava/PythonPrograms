41559753137d619cddc81203daaea622631d5b55
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-10-10 23:08:34 -0700

    New File

---
+++ Starting with turtles.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.showturtle()
+turtle.write("Joana Mucogllava")
+turtle.forward(100)
+turtle.right(90)
+turtle.color("blue")
+turtle.forward(50)
+turtle.right(90)
+turtle.color("purple")
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(80)
