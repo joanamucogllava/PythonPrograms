0cbf078d7a8cb405be144141e1862227df395c73
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-10-10 23:49:13 -0700

    Update radius.py

--- radius.py
+++ radius.py
@@ -1,3 +1,5 @@
+# This program will evaluate the area of the cicle in radius.
+
 radius = eval(input("Enter a value for radius: "))
 area = radius * radius * 3.14159
 print("The area for the circle of radius", radius, "is", area)
