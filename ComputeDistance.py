b04ff8cda399847c7b4e5b3c6fc182459493c5a1
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-10-12 09:21:49 -0700

    New File

---
+++ ComputeDistance.py
@@ -0,0 +1,4 @@
+x1, y1 = eval(input("Enter x1 and y1 for Point1: "))
+x2, y2 = eval(input("Enter x2 and y2 for Point2: "))
+distance = ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) ** 0.5
+print("The distance between the two points is", distance)
