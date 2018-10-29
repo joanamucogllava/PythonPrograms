41f2ced252aecfdde584dea6806dec1cab5516fc
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-10-29 15:39:56 -0700

    New Project

---
+++ documents/Simple.Shapes.py
@@ -0,0 +1,30 @@
+import turtle
+turtle.pensize(3)
+turtle.penup()
+turtle.goto(-200, -50)
+turtle.pendown()
+turtle.circle(40, steps = 3) #triangle shape
+
+turtle.penup()
+turtle.goto(-100, -50)
+turtle.pendown()
+turtle.circle(40, steps = 4) #square shape
+
+turtle.penup()
+turtle.goto(0, -50)
+turtle.pendown()
+turtle.circle(40, steps = 5) #pentagon shape
+
+turtle.penup()
+turtle.goto(100, -50)
+turtle.pendown()
+turtle.circle(40, steps = 6) #hexagon shape
+
+turtle.penup()
+turtle.goto(200, -50)
+turtle.pendown()
+turtle.circle(40) #circle shape
+
+turtle.done()
+
+
