1af1cf0af85cbd8d3b70a086bc553b317f665192
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-10-30 19:45:16 -0700

    New program

---
+++ documents/AdditionQuiz.py
@@ -0,0 +1,10 @@
+import random
+
+number1 = random.randint(0, 9)
+number2 = random.randint(0, 9)
+
+answer = eval(input("What is " + str(number1) + " + "
+    + str(number2) + "? "))
+
+print(number1, "+", number2, "=", answer,
+    "is", number1 + number2 == answer)
