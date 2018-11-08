ec12341369e68709d62df5f36dff9f12a0630b43
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-11-07 16:11:57 -0800

    New Program

---
+++ documents/SentinelValue.py
@@ -0,0 +1,11 @@
+data = eval(input("Enter an integer (the input ends " +
+    "if it is 0): "))
+
+sum = 0
+while data != 0:
+    sum += data
+
+    data = eval(input("Enter an integer (the input ends " +
+        "if it is 0): "))
+
+print("The sum is", sum)
