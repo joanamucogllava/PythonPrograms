054efe7b998aec25253fa507cd220e79a28c67b5
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-11-16 20:19:34 -0800

    New File

---
+++ documents/GCDFunction.py
@@ -0,0 +1,17 @@
+def gcd(n1, n2):
+    gcd = 1
+    k = 2
+
+    while k <= n1 and k <= 2:
+        if n1 % k == 0 and n2 % k == 0:
+            gcd = k
+        k += 1
+
+    return gcd
+
+
+n1 = eval(input("Enter the first integer: "))
+n2 = eval(input("Enter the second integer: "))
+
+print("The greatest common divisior from", n1,
+      "and", n2, "is", gcd(n1, n2))
