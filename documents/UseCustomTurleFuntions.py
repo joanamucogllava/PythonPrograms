a14d3a02022afd7d09161ac82dcdbda1643bbdeb
Joana Mucogllava <mucogllavajoana@hotmail.com>
2018-11-19 21:46:52 -0800

    New File

---
+++ documents/UseCustomTurleFuntions.py
@@ -0,0 +1,15 @@
+import turtle
+from UsefulTurtleFunctions import *
+
+drawLine(-50, -50, 50, 50)
+
+writeText("Testing useful Turtle functions", -50, -60)
+
+drawPoint(0, 0)
+
+drawCircle(0, 0, 80)
+
+drawRectangle(0, 0, 60, 40)
+
+turtle.hideturtle()
+turtle.done()
