752d541a7ce785676aba663703fdd5dba11e67b3
Joana Mucogllava <mucogllavajoana@hotmail.com>
2019-07-18 08:57:15 -0700

    Update 100years.py

--- 100years.py
+++ 100years.py
@@ -1,5 +1,5 @@
 #This program will ask for your name and age and will tell you at what age you will turn 100 years old.
-
+# This is a practice program.
 name = str(input("What is your name?"))
 age = int(input("What is your age?"))
 thisyear = int(input("what year is now?"))
